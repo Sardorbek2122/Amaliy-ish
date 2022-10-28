ef53f343ac6ce828c54cace5e10c5d7ceec2515f
Sardorbek2122 <116882060+Sardorbek2122@users.noreply.github.com>
2022-10-28 15:14:25 +0500

    Add files via upload

---
+++ Amaliy ish 2122.py
@@ -0,0 +1,12 @@
+# 1
+F=int(input('Farengeytni kiriting: '))
+S=(F-32)*5/9
+print (f'Selsiy={S} ')
+
+# 1.1
+v1=int(input('Birinchi avtomobil tezligini kiriting: '))
+v2=int(input('Ikkinchi avtomobil tezligini kiriting: '))
+T=int(input('Vaqtni kiriting: '))
+S=(v1*v2*T)
+print(f'Ular orasidagi masofa={S} km')
+
